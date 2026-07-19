d8099e841c402064ae18a1f25b9cc6d32bcbcf6f
Alwin Dison R <allwina060@gmail.com>
2026-07-19 10:47:53 +0530

    Update stars.js

--- js/stars.js
+++ js/stars.js
@@ -1,56 +1,46 @@
 const canvas = document.getElementById("stars");
 const ctx = canvas.getContext("2d");
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-let stars = [];
-
-for(let i=0;i<200;i++){
-
-    stars.push({
-
-        x:Math.random()*canvas.width,
-        y:Math.random()*canvas.height,
-        r:Math.random()*2,
-        s:Math.random()*0.5+0.2
-
-    });
-
+function resize() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 }
 
-function animate(){
+resize();
 
-    ctx.clearRect(0,0,canvas.width,canvas.height);
+const stars = [];
 
-    ctx.fillStyle="white";
+for (let i = 0; i < 150; i++) {
+    stars.push({
+        x: Math.random() * canvas.width,
+        y: Math.random() * canvas.height,
+        r: Math.random() * 3 + 1,
+        speed: Math.random() * 0.5 + 0.2
+    });
+}
 
-    stars.forEach(star=>{
+function animate() {
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (const star of stars) {
 
         ctx.beginPath();
-        ctx.arc(star.x,star.y,star.r,0,Math.PI*2);
+        ctx.arc(star.x, star.y, star.r, 0, Math.PI * 2);
+        ctx.fillStyle = "#ffffff";
         ctx.fill();
 
-        star.y+=star.s;
+        star.y += star.speed;
 
-        if(star.y>canvas.height){
-
-            star.y=0;
-            star.x=Math.random()*canvas.width;
-
+        if (star.y > canvas.height) {
+            star.y = 0;
+            star.x = Math.random() * canvas.width;
         }
-
-    });
+    }
 
     requestAnimationFrame(animate);
-
 }
 
 animate();
 
-window.addEventListener("resize",()=>{
-
-    canvas.width=window.innerWidth;
-    canvas.height=window.innerHeight;
-
-});
+window.addEventListener("resize", resize);
